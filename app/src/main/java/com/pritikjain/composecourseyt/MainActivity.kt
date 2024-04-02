5f3219be7984a116a098c99005026a0ef99dad56
Priti Jain <pritik.jain@gmail.com>
2024-04-02 15:39:25 -0700

    Modifier - Android Jetpack Compose part 3

--- app/src/main/java/com/pritikjain/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/pritikjain/composecourseyt/MainActivity.kt
@@ -4,12 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,18 +35,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    //.fillMaxSize(0.5f)
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green)
+                    .fillMaxWidth()
+
+                   // .requiredWidth(600.dp),
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp,Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp,Color.Red)
+                    .padding(10.dp)
             ) {
-                Text("Hello")
+                Text("Hello", modifier = Modifier.clickable {
+
+                })
+                Spacer(modifier = Modifier.height(50.dp) )
                 Text("World")
-                Text("Priti")
             }
 
         }
